6748d3c2a780bc39a10b9aaf240448a159b9ab26
jonwynn <143842244+jonwynn@users.noreply.github.com>
2024-10-01 22:44:19 -0500

    Add files via upload
    Fixed menu to use every sorting algorithm by utilizing the dictionaries in main, and also implemented timsort. There are a few new functions for it, including a modified merge sort called tim_merge, an insertion sort function, and tim_sort.

--- AlgorithmsProject2.py
+++ AlgorithmsProject2.py
@@ -81,6 +81,105 @@ if complexity_selection == '4':
 import time
 import random
 
+TIM_MIN_RUN = 32
+    
+"""
+    The main idea behind Tim Sort is to exploit the existing 
+    order in the data to minimize the number of comparisons 
+    and swaps. It achieves this by dividing the array into 
+    small subarrays called runs, which are already sorted, 
+    and then merging these runs using a modified merge sort 
+    algorithm.
+"""
+# Calculates minimum run
+def calculate_minRun(n):
+    run = 0
+    while n >= TIM_MIN_RUN:
+        run |= n & 1 
+        n >>= 1 
+    return n + run
+
+
+# This function sorts array from left index to 
+# to right index which is of size atmost RUN 
+def insertion_sort(insArray, left, right):
+    for i in range(left + 1, right + 1):
+        j = i
+        while j > left and insArray[j] < insArray[j - 1]:
+            insArray[j], insArray[j - 1] = insArray[j - 1], insArray[j]
+            j -= 1
+
+# Merge function merges the sorted runs
+def tim_merge(arr, l, m, r):
+    len1, len2 = m - l + 1, r - m
+    left, right = [], [] 
+    for i in range(0, len1): 
+        left.append(arr[int(l + i)]) 
+    for i in range(0, len2): 
+        right.append(arr[int(m + 1 + i)]) 
+  
+    i, j, k = 0, 0, l
+    
+    # after comparing, we merge those two array 
+    # in larger sub array 
+    while i < len1 and j < len2: 
+        if left[i] <= right[j]: 
+            arr[k] = left[i] 
+            i += 1
+        else: 
+            arr[k] = right[j] 
+            j += 1
+        k += 1
+  
+    # Copy remaining elements of left, if any 
+    while i < len1: 
+        arr[k] = left[i] 
+        k += 1
+        i += 1
+  
+    # Copy remaining element of right, if any 
+    while j < len2: 
+        arr[k] = right[j] 
+        k += 1
+        j += 1
+    
+
+def tim_sort(data): 
+    start_time = time.time()
+    n = len(data) 
+    minRun = calculate_minRun(n) 
+  
+    # Sort individual subarrays of size RUN 
+    for start in range(0, n, minRun): 
+        end = min(start + minRun - 1, n - 1) 
+        insertion_sort(data, start, end) 
+  
+    # Start merging from size RUN (or 32 being the minimum). It will merge 
+    # to form size 64, then 128, 256 and so on .... 
+    size = minRun 
+    while size < n: 
+  
+        # Pick starting point of left sub array,
+        # merge arr[left..left + size - 1] and
+        # arr[left + size, left + 2 * size -1] 
+        # After every merge, we increase left by 2 * size 
+        for left in range(0, n, 2 * size): 
+  
+            # Find ending point of left sub array 
+            # mid+1 is starting point of right sub array 
+            mid = min(n - 1, left + size - 1) 
+            right = min((left + 2 * size - 1), (n - 1)) 
+  
+            # Merge sub array arr[left.....mid] & 
+            # arr[mid + 1....right] 
+            if mid < right: 
+                tim_merge(data, left, mid, right) 
+  
+        size = 2 * size 
+    end_time = time.time()
+    time_taken = end_time - start_time
+    return time_taken, data
+
 def bubble_sort(data):
     # bubble sort loops through the list and pushes the biggest number to the top
     # it does this a number of times equal to the size of the list -1 
@@ -103,7 +202,7 @@ def bubble_sort(data):
     
 def merge_sort(data):
     """ Enter function info """
-
+    start_time = time.time()
     # Base case
     if len(data) <= 1:
         return data
@@ -123,8 +222,9 @@ def merge_sort(data):
     # Skip merge if largest value in left half is <= smallest value in right
     if leftHalf[-1] <= rightHalf[0]:
         return leftHalf + rightHalf
-
-    return merge(leftHalf,rightHalf)
+    end_time = time.time()
+    time_taken = end_time - start_time
+    return time_taken, merge(leftHalf,rightHalf)
 
 def merge(left, right):
     """merge function here"""
@@ -157,11 +257,7 @@ def quick_sort(data):
     return quick_sort(left) + middle + quick_sort(right)
     # Continue
 
-def tim_sort(data):
-    """ Enter function info """
-    print("Timsort is running...")
-    print("")
-    # Continue
+
      
 """
 Suggestion from Jon - make gen_data its own function 
@@ -170,22 +266,23 @@ instead of inside of gen_data. Will make implementation
 easier to do and follow.
 
 Record the time taken to run the sort in the same function as
-the sorting algorithm instead of a seperate function
+the sorting algorithm instead of a seperate func1tion
 
 """
 
 # Generates data and runs sorting function
 def gen_data(size, complexity_selection):
     """ Enter function info """
-    data = []
-    if complexity_selection == '1': # Best case
-        data = [i for i in range(size)]
-    elif complexity_selection == '2': # Average case
-        data = [random.randint(0,size) for x in range(size)]
-    elif complexity_selection == '3': # Worst case
-        data = [i for i in range(size, 0, -1)]
-    else:
-        raise ValueError("Invalid selection!")
+    if (complexity_selection == '1' or '2' or '3'):
+        data = []
+        if complexity_selection == '1': # Best case
+            data = [i for i in range(size)]
+        elif complexity_selection == '2': # Average case
+            data = [random.randint(0,size) for x in range(size)]
+        elif complexity_selection == '3': # Worst case
+            data = [i for i in range(size, 0, -1)]
+        else:
+            raise ValueError("Invalid selection!")
 
     # Make a copy to avoid modifying original data
     data_to_sort = data.copy()
@@ -301,35 +398,49 @@ def main():
             
             # complexity options is another dictionary defined at top of main
             
-            # best case
+            if complexity_selection == '4':
+                break
+            
+            
+            # bubble_sort
+            # merge_sort
+            # quick_sort
+            # case complexity selection from user
             if complexity_selection == '1' or '2' or '3':
-                
+                # this is a test
                 unsorted_data = gen_data(100, complexity_selection)
-                time_to_display, sorted_data = bubble_sort(unsorted_data)
+                time_to_display, sorted_data = ((sorting_algorithms[sort_selection])(unsorted_data))
+                # time_to_display, sorted_data = bubble_sort(unsorted_data)
                 print(f"For N = 100,     it takes {time_to_display:.6f} seconds")
                 
                 unsorted_data = []
                 unsorted_data = gen_data(1000, complexity_selection)
-                time_to_display, sorted_data = bubble_sort(unsorted_data)
+                time_to_display, sorted_data = ((sorting_algorithms[sort_selection])(unsorted_data))
                 print(f"For N = 1000,    it takes {time_to_display:.6f} seconds")
                 
                 unsorted_data = []
                 unsorted_data = gen_data(10000, complexity_selection)
-                time_to_display, sorted_data = bubble_sort(unsorted_data)
+                time_to_display, sorted_data = ((sorting_algorithms[sort_selection])(unsorted_data))
                 print(f"For N = 10000,   it takes {time_to_display:.6f} seconds")
                 print("")
                 
                 yes_no = input("Do you want to input another N (Y/N)? ")
+                
+                if (yes_no == "N"):
+                    break
+                
+                # there was an if statement here instead of a while, making the menu not loop
+                # back like it was supposed to after selecting Y / N
                 while (yes_no == "Y"):
                     new_Size = int(input("What is the N? "))
                     print("")
                     unsorted_data = []
                     unsorted_data = gen_data(new_Size, complexity_selection)
-                    time_to_display, sorted_data = bubble_sort(unsorted_data)
+                    time_to_display, sorted_data = ((sorting_algorithms[sort_selection])(unsorted_data))
                     print(f"For N = {new_Size}, it takes {time_to_display:.6f} seconds")
                     print("")
                     
-                    yes_no = input("Do you want to input another N (Y/N)?")
+                    yes_no = input("Do you want to input another N (Y/N)? ")
                 
             elif complexity_selection == '4':
                 print("Exiting case selection.")
